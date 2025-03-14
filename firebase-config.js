f4b17e051d84b4b4f34317c47506819a2164599f
ARodHume <pagnoni.andrea@gmail.com>
2025-03-14 17:08:48 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+        // firebase-config.js
+export const firebaseConfig = {
+  apiKey: "IL-TUO-API-KEY",
+  authDomain: "tuo-progetto.firebaseapp.com",
+  projectId: "tuo-progetto",
+  storageBucket: "tuo-progetto.appspot.com",
+  messagingSenderId: "tuo-numero",
+  appId: "tuo-app-id"
+};
